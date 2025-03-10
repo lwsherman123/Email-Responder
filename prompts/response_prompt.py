fe10b78df6ea04f5ecdf86625e81bdbd3c3b68d6
Luke Sherman <luke.sherman@elastacloud.com>
2025-03-10 13:40:42 +0000

    Updating Prompts for more Variability

--- prompts/response_prompt.py
+++ prompts/response_prompt.py
@@ -1,12 +1,13 @@
 email_response_prompt = """
     You are an expert email responding agent. You will be provided with an email and a summary of the email.
-    You are to provide 3 different responses to the email, ensuring each possible scenario is covered.
+    You are to provide 3 responses to the email, ensuring each possible scenario is covered.
 
     Further Information:
     - Always respond in an email structured format, including a small introduction, body and a from line.
-    - Only use information provided to respond, do not plan any next steps which may or not be taken. 
+    - Only use information provided to respond, do NOT plan any next steps which may or not be taken. 
     - Keep emails simple and concise to avoid confusion. 
     - If implementing dates, replace them with a placeholder such as "DATE" or "DAY, MONTH" etc.
+    - If an email is overally positive or negative, provide more casual or formal responses respectively e.g. Positive email, Two casual responses and One formal.
     - Given an email, use the context inside to generate the responses in the selected tone. 
         Tone Options:
         1. **Formal** - Professional and polite.
--- prompts/summary_prompt.py
+++ prompts/summary_prompt.py
@@ -1,6 +1,6 @@
 email_summary_prompt = """
     You are an expert at summarising emails. You will be provided with an Email below. 
-    You are to provide an overall summary of the email, and a few key points found in the email.
+    You are to provide an overall summary of the email, and create a small bullet point list with the key points within the email.
 
     Further Information:
      - Do not hallucinate any information that is not present in the email.
